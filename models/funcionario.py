70b6410bb65d10d21c816c26372a393598a32d4f
voituregestaodeestoque <voituregestaodeestoque@gmail.com>
2026-05-12 16:50:02 -0300

    att

--- models/funcionario.py
+++ models/funcionario.py
@@ -28,26 +28,27 @@ class Funcionario(CrudBase):
         self.funcionario_telefone = funcionario_telefone
         self.funcionario_cargo = funcionario_cargo
     
+    #Função para validar os campos da tabela 
     def validate(self):
-        erros = [
-            Validator.validar_nome(self.funcionario_nome, "nome"),            
-            Validator.validar_ddi_ddd(self.funcionario_ddi, "ddi"),
-            Validator.validar_ddi_ddd(self.funcionario_ddd, "ddd"),
-            Validator.validar_telefone(self.funcionario_telefone, "telefone")
-        ]
-        return [erro for erro in erros if erro]
+        erros = []
 
-    @classmethod
-    def login(cls):
-        conexao = Database.connect()
-        cursor = conexao.cursor(dictionary=True)
-        try:
-            sql = "select u.*, f.funcionario_nome, e.* from uso_empilhadeira as u inner join funcionario as f on u.funcionario_id = f.id inner join empilhadeira as e on u.empilhadeira_id= e.id;"
-            cursor.execute(sql)
-            return cursor.fetchall()
-        finally:
-            cursor.close()
-            conexao.close()
+        validacoes = [
+            Validator.validar_nome(self.funcionario_nome, "funcionario_nome"),
+            Validator.validar_senha(self.funcionario_senha, "funcionario_senha"),
+            Validator.validar_cpf_cnpj(self.funcionario_cpf, "funcionario_cpf"),
+            Validator.validar_cep(self.funcionario_cep, "funcionario_cep"),
+            Validator.validar_email(self.funcionario_email, "funcionario_email"),
+            Validator.validar_ddi_ddd(self.funcionario_ddi, "funcionario_ddi"),
+            Validator.validar_ddi_ddd(self.funcionario_ddd, "funcionario_ddd"),
+            Validator.validar_telefone(self.funcionario_telefone, "funcionario_telefone"),
+            Validator.validar_cargo(self.funcionario_cargo, "funcionario_cargo")
+        ]
+        
+        for itens in validacoes:
+            if not itens['valida']:
+                erros.append(itens["mensagem"])
+
+        return erros
 
     @classmethod
     def login(cls, email, senha):
@@ -62,6 +63,73 @@ class Funcionario(CrudBase):
                 return usuario
             
             return None
+        finally:
+            cursor.close()
+            conexao.close()
+
+    #Função para listar todos os funcionarios registrados
+    @classmethod
+    def funcionario_listagem(cls):
+        conexao = Database.connect()
+        cursor = conexao.cursor(dictionary=True)
+        try:
+            sql = "SELECT * FROM funcionario"
+            cursor.execute(sql)
+            return cursor.fetchall()
+        finally:
+            cursor.close()
+            conexao.close()
+
+
+    #Função para deletar com segurança
+    @classmethod
+    def safe_delete(cls, id):
+        fornecedor = cls.find_by_id(id)
+        if not fornecedor:
+            raise ValueError("Funcionario não encontrado.")
+        if cls.has_related_records(id):
+            raise ValueError("Não é possível excluir o funcionario porque ele está vinculado a outros serviços.")
+        cls.delete(id)
+
+    #Procura se o funcionario em questão está relacionado com algum pedido de entrada
+    @classmethod
+    def has_related_records(cls, id):
+        conexao = Database.connect()
+        cursor = conexao.cursor()
+        try:
+            queries = [
+                "SELECT COUNT(*) FROM uso_empilhadeira WHERE funcionario_id = %s"
+            ]
+            total = 0
+            for sql in queries:
+                cursor.execute(sql, (id,))
+                total += cursor.fetchone()[0]
+            return total > 0
+        finally:
+            cursor.close()
+            conexao.close()
+
+    
+    @classmethod
+    def find_by_cpf(cls, cpf):
+        conexao = Database.connect()
+        cursor = conexao.cursor(dictionary=True)
+        try:
+            sql = f"SELECT * FROM {cls.table} WHERE funcionario_cpf = %s"
+            cursor.execute(sql, (cpf,))
+            return cursor.fetchone()
+        finally:
+            cursor.close()
+            conexao.close()
+
+    @classmethod
+    def find_by_email(cls, email):
+        conexao = Database.connect()
+        cursor = conexao.cursor(dictionary=True)
+        try:
+            sql = f"SELECT * FROM {cls.table} WHERE funcionario_email = %s"
+            cursor.execute(sql, (email,))
+            return cursor.fetchone()
         finally:
             cursor.close()
             conexao.close()
