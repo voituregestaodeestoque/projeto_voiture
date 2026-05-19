9ff18d0df164234339430adf14208b402d98eb2a
RyanRIbeiroGamer-_- <ryan.r.silva22@aluno.senai.br>
2026-05-19 10:09:11 -0300

    estoque.py classe

---
+++ models/estoque.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from core.crud_base import CrudBase
+from core.database import Database
+from core.validator import Validator
+
+class Estoque(CrudBase):
+    table = "estoque"
+    fields = [
+        "estoque_quantidade",
+        "produto_id"]
+
+    def __init__(self, estoque_quantidade, produto_id):
+        self.estoque_quantidade = estoque_quantidade
+        self.produto_id = produto_id
+
+    #Função para validar os campos da tabela 
+    def validate(self):
+        erros = []
+
+        validacoes = [
+            Validator.validar_quantidade(self.estoque_quantidade, "estoque_quantidade")
+        ]
+        
+        for itens in validacoes:
+            if not itens['valida']:
+                erros.append(itens["mensagem"])
+
+        return erros
