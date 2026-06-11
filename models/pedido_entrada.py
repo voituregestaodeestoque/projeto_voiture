42e916db550692d72de157422c1838d19a24a7ed
mclarabelli <maria.belli@aluno.senai.br>
2026-06-11 13:40:59 -0300

    atualizacao

--- models/pedido_entrada.py
+++ models/pedido_entrada.py
@@ -58,13 +58,12 @@ class Pedido_entrada(CrudBase):
         cursor = conexao.cursor(dictionary=True)
 
         try:
-            sql = """SELECT p.id as detalhe_entrada_id, p.status_pedido_entrada, p.data_pedido_entrada, p.fornecedor_id, pr.produto_nome, de.detalhe_entrada_quantidade, 
+            sql = """SELECT p.id as pedido_entrada_id, p.status_pedido_entrada, p.data_pedido_entrada, p.fornecedor_id, pr.produto_nome, de.detalhe_entrada_quantidade, 
                     MAX(me.datahora_movimentacao_entrada) AS data_processamento
                     FROM pedido_entrada p
                     LEFT JOIN detalhe_entrada de ON p.id = de.pedido_entrada_id
                     LEFT JOIN movimentacao_entrada me ON de.id = me.detalhe_entrada_id AND de.pedido_entrada_id = me.detalhe_entrada_pedido_entrada_id
-                    left join estoque es on es.id = de.estoque_id
-                    LEFT JOIN produto pr ON pr.id = es.produto_id
+                    LEFT JOIN produto pr ON pr.id = de.produto_id
                     GROUP BY p.id, p.status_pedido_entrada, p.fornecedor_id, pr.produto_nome, de.detalhe_entrada_quantidade
                     ORDER BY p.id DESC"""
             cursor.execute(sql)
@@ -106,8 +105,12 @@ class Pedido_entrada(CrudBase):
             for item in itens:
 
                 cursor.execute(
-                "SELECT * FROM estoque WHERE id = %s",
-                    (item["estoque_id"],))
+                """
+                SELECT *
+                FROM estoque
+                WHERE produto_id = %s
+                """,
+                (item["produto_id"],))
 
                 estoque = cursor.fetchone()
                 if not estoque:
@@ -120,26 +123,16 @@ class Pedido_entrada(CrudBase):
                 """ 
                     UPDATE estoque
                     SET estoque_quantidade = %s
-                    WHERE id = %s
+                    WHERE produto_id = %s
                     """,
-                    (nova_quantidade, item["estoque_id"]) )
-
-                cursor.execute(
-                    """
-                    INSERT INTO movimentacao_entrada 
-                    (datahora_movimentacao_entrada, detalhe_entrada_id, detalhe_entrada_pedido_entrada_id)
-                    VALUES (%s, %s, %s)
-                    """,
-                    (datetime.now(), item["id"], pedido_entrada_id)
-                )
+                    (nova_quantidade, item["produto_id"]) )
 
             cursor.execute(
                 """
                 UPDATE pedido_entrada
-                SET status_pedido_entrada = %s
                 WHERE id = %s
                 """,
-                ("FINALIZADO", pedido_entrada_id)
+                (pedido_entrada_id)
             )
 
             conexao.commit()
@@ -159,7 +152,6 @@ class Pedido_entrada(CrudBase):
         cursor = conexao.cursor(dictionary=True)
 
         try:
-            # Faz o JOIN para trazer a coluna "fornecedor" que o seu HTML precisa
             sql = """SELECT 
                         p.id, 
                         p.status_pedido_entrada,
@@ -170,39 +162,96 @@ class Pedido_entrada(CrudBase):
                     INNER JOIN fornecedor f ON p.fornecedor_id = f.id
                     WHERE p.id = %s"""
             cursor.execute(sql, (pedido_entrada_id,))
-            return cursor.fetchone() # Retorna o pedido com o nome do fornecedor incluso
+            return cursor.fetchone() 
         finally:
             cursor.close()
             conexao.close()
 
     
     @classmethod
-    def processar(cls, id):
+    def processar(cls, pedido_entrada_id):
         conexao = Database.connect()
         cursor = conexao.cursor(dictionary=True)
         try:
             conexao.start_transaction()
 
-            cursor.execute("SELECT * FROM pedido_entrada WHERE id = %s FOR UPDATE", (id,))
+            cursor.execute("SELECT * FROM pedido_entrada WHERE id = %s FOR UPDATE", (pedido_entrada_id,))
             pedido = cursor.fetchone()
             if not pedido:
-                raise ValueError("Pedido não encontrado.")
+                raise ValueError("Pedido de entrada não encontrado.")
 
             if pedido["status_pedido_entrada"] != "PENDENTE":
                 raise ValueError("Somente pedidos pendentes podem ser processados.")
 
-            cursor.execute("SELECT * FROM produto WHERE id = %s FOR UPDATE", (pedido["produto_id"],))
-            produto = cursor.fetchone()
-            if not produto:
-                raise ValueError("Produto não encontrado.")
+            cursor.execute("SELECT * FROM detalhe_entrada WHERE pedido_entrada_id = %s FOR UPDATE", (pedido_entrada_id,))
 
+            detalhes = cursor.fetchall()
+
+            if not detalhes:
+                raise ValueError("Pedido sem itens.")
+
+            for detalhe in detalhes:
+
+         
+                cursor.execute(
+                    """
+                    SELECT *
+                    FROM estoque
+                    WHERE produto_id = %s
+                    FOR UPDATE
+                    """,
+                    (detalhe["produto_id"],)
+                )
+                estoque = cursor.fetchone()
+
+                if not estoque:
+                    raise ValueError(
+                        f"Não existe estoque para o produto {detalhe['produto_id']}"
+                    )
+
+                nova_quantidade = (
+                    estoque["estoque_quantidade"]
+                    + detalhe["detalhe_entrada_quantidade"]
+                )                
+         
+                cursor.execute(
+                    """
+                    UPDATE estoque
+                    SET estoque_quantidade = %s
+                    WHERE produto_id = %s
+                    """,
+                    (
+                        nova_quantidade,
+                        detalhe["produto_id"]
+                        )
+                )
+
+            
+                cursor.execute(
+                    """
+                    INSERT INTO movimentacao_entrada
+                    (
+                        datahora_movimentacao_entrada,
+                        detalhe_entrada_id,
+                        detalhe_entrada_pedido_entrada_id
+                    )
+                    VALUES (%s, %s, %s)
+                    """,
+                    (
+                        datetime.now(),
+                        detalhe["id"],
+                        pedido_entrada_id
+                    )
+                )
+
+    
             cursor.execute(
                 """
                 UPDATE pedido_entrada
-                SET status = %s, data_processamento = %s
+                SET status_pedido_entrada = %s
                 WHERE id = %s
                 """,
-                ("CONCLUIDO", datetime.now(), id)
+                ("CONCLUIDO", pedido_entrada_id)
             )
 
             conexao.commit()
@@ -214,32 +263,3 @@ class Pedido_entrada(CrudBase):
             cursor.close()
             conexao.close()
 
-    @classmethod
-    def cancelar(cls, id):
-        conexao = Database.connect()
-        cursor = conexao.cursor(dictionary=True)
-        try:
-            cursor.execute("SELECT * FROM pedido_movimentacao WHERE id = %s", (id,))
-            pedido = cursor.fetchone()
-            if not pedido:
-                raise ValueError("Pedido não encontrado.")
-            if pedido["status"] != "PENDENTE":
-                raise ValueError("Somente pedidos pendentes podem ser cancelados.")
-
-            cursor = conexao.cursor()
-            cursor.execute(
-                """
-                UPDATE pedido_movimentacao
-                SET status = %s, data_processamento = %s
-                WHERE id = %s
-                """,
-                ("CANCELADO", datetime.now(), id)
-            )
-            conexao.commit()
-            return "Pedido cancelado com sucesso."
-        except Exception:
-            conexao.rollback()
-            raise
-        finally:
-            cursor.close()
-            conexao.close()
