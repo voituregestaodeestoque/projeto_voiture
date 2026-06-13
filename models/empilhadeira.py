b120b791f5deb81669ad60a82e360c80f967f025
RyanRIbeiroGamer-_- <ryan.r.silva22@aluno.senai.br>
2026-06-13 18:37:48 -0300

    comentarios de algumas telas e algumas classes

--- models/empilhadeira.py
+++ models/empilhadeira.py
@@ -1,16 +1,19 @@
-from datetime import datetime
+#faz todas as importações que iremos utilizar na classe empilhadeira
 from core.crud_base import CrudBase
 from core.database import Database
 from core.validator import Validator
 
-class Empilhadeira(CrudBase):
-    table = "empilhadeira"
+class Empilhadeira(CrudBase): #cria a classe empilhadeira
+    table = "empilhadeira" #nome da tabela = empilhadeira
     fields = [
         "empilhadeira_chassi",
         "empilhadeira_status",
         "empilhadeira_modelo",
         "empilhadeira_marca"]
-
+    
+    #campos que existe na tabela
+    
+    #metodo pra criar um objeto e no self passando os dados de cada campo
     def __init__(self, empilhadeira_chassi, empilhadeira_status="INATIVA", empilhadeira_modelo="", empilhadeira_marca=""):
         self.empilhadeira_chassi = empilhadeira_chassi
         self.empilhadeira_status = empilhadeira_status
@@ -21,23 +24,26 @@ class Empilhadeira(CrudBase):
     def validate(self):
         erros = []
 
+        #valida cada campo da tabela
         validacoes = [
             Validator.validar_chassi(self.empilhadeira_chassi, "empilhadeira_chassi"),
             Validator.validar_modelo(self.empilhadeira_modelo, "empilhadeira_modelo"),
             Validator.validar_marca(self.empilhadeira_marca, "empilhadeira_marca")
         ]
-        
+        #se der algum erro
         for itens in validacoes:
             if not itens['valida']:
                 erros.append(itens["mensagem"])
 
         return erros
 
+    #função que mostra as empilhadeiras que estão sendo utilizadas
     @classmethod
     def tabelatudojunto(cls):
-        conexao = Database.connect()
-        cursor = conexao.cursor(dictionary=True)
+        conexao = Database.connect() #conecta no banco
+        cursor = conexao.cursor(dictionary=True) #objeto
         try:
+            #passa o select correto pra mostrar as empilhadeiras que estão sendo utilizadas
             sql = sql = """SELECT
     u.id AS uso_id,
     u.uso_empilhadeira_datahora,
@@ -59,64 +65,70 @@ INNER JOIN empilhadeira e
     ON u.empilhadeira_id = e.id
 WHERE u.funcionario_id IS NOT NULL
 """
-            cursor.execute(sql)
-            return cursor.fetchall()
-        finally:
+            cursor.execute(sql) #executa o comando select
+            return cursor.fetchall() #retorna as informações após o comando do sql
+        finally: #fecha o objeto e a conexão
             cursor.close()
             conexao.close()
 
+    #função que mostra as empilhadeiras que não estão sendo utilizadas
     @classmethod
     def empilhadeirasemuso(cls):
-        conexao = Database.connect()
-        cursor = conexao.cursor(dictionary=True)
+        conexao = Database.connect() #conecta no banco
+        cursor = conexao.cursor(dictionary=True) #objeto
         try:
+            #passa o select correto pra mostrar as empilhadeiras que não estão sendo utilizadas
             sql = """ SELECT e.* FROM empilhadeira e LEFT JOIN uso_empilhadeira u ON e.id = u.empilhadeira_id AND u.funcionario_id IS NOT NULL WHERE u.id IS NULL """
-            cursor.execute(sql)
-            return cursor.fetchall()
-        finally:
+            cursor.execute(sql) #executa o comando sql do select
+            return cursor.fetchall() #retorna os dados após o comando select
+        finally: # fecha o objeto e a conexão
             cursor.close()
             conexao.close()
 
     #Função para deletar com segurança
     @classmethod
     def safe_delete(cls, id):
-        empilhadeira = cls.find_by_id(id)
-        if not empilhadeira:
+        empilhadeira = cls.find_by_id(id) #procura o id da empilhadeira
+        if not empilhadeira: #se não achar o id da empilhadeira
             raise ValueError("Empilhadeira não encontrada.")
-        if cls.has_related_records(id):
+        if cls.has_related_records(id): #verifica se essa empilhadeira está vinculada em mais de uma tabela
             raise ValueError("Não é possível excluir a empilhadeira porque ele está vinculado a outros serviços.")
-        cls.delete(id)
+        cls.delete(id) #deleta
 
+    #função que verifica se a empilhadeira está em outra tabela
     @classmethod
     def has_related_records(cls, id):
-        conexao = Database.connect()
-        cursor = conexao.cursor()
+        conexao = Database.connect() #conecta no banco
+        cursor = conexao.cursor() 
         try:
+            #conta quantas vezes existe o campo empilhadeira_id na tabela uso_emilhadeira
             queries = [
                 "SELECT COUNT(*) FROM uso_empilhadeira WHERE empilhadeira_id = %s"
             ]
             total = 0
-            for sql in queries:
-                cursor.execute(sql, (id,))
-                total += cursor.fetchone()[0]
-            return total > 0
-        finally:
+            for sql in queries: #percorre todos os comandos de dentro a tabela, porém nesse caso, só uma vez
+                cursor.execute(sql, (id,)) #executa cada comando passando o id
+                total += cursor.fetchone()[0] #pega a quantidade de vezes que encontrou do retorno e soma no total
+            return total > 0 #se for maior que zero=True | se for menor que zero=False
+        finally: # fecha o objeto e a conexão
             cursor.close()
             conexao.close()
 
     #Procura no banco alguma empilhadeira com o mesmo chassi
     @classmethod
     def chassi_existente(cls, chassi):
-        conexao = Database.connect()
-        cursor = conexao.cursor(dictionary=True, buffered=True)
+        conexao = Database.connect() #conecta no banco
+        cursor = conexao.cursor(dictionary=True, buffered=True) #objeto
         try:
+            #procura se existe uma empilhadeira com um chassi especifico
             sql = f"SELECT * FROM {cls.table} WHERE empilhadeira_chassi = %s"
-            cursor.execute(sql, (chassi,))
-            return cursor.fetchone()
-        finally:
+            cursor.execute(sql, (chassi,)) #executa o query
+            return cursor.fetchone() #retorna o resultado da query
+        finally: # fecha o objeto e a conexão
             cursor.close()
             conexao.close()
 
     @classmethod
+    #função para listar todas as empilhadeiras
     def empilhadeira_listagem(cls):
-            return cls.find_all()
+        return cls.find_all()
--- models/funcionario.py
+++ models/funcionario.py
@@ -1,11 +1,12 @@
+#faz todas as importações que iremos utilizar na classe funcionario
 from core.crud_base import CrudBase
 from core.database import Database
 from core.validator import Validator
 from core.security import gerar_hash_senha, verificar_senha
 
 
-class Funcionario(CrudBase):
-    table='funcionario'
+class Funcionario(CrudBase): #cria a classe empilhadeira
+    table='funcionario' #nome da tabela
     fields=[
         'funcionario_nome',
         'funcionario_senha',
@@ -17,7 +18,9 @@ class Funcionario(CrudBase):
         'funcionario_telefone',
         'funcionario_cargo'
     ]
+    #campos que existe na tabela
 
+    #metodo pra criar um objeto e no self passando os dados de cada campo
     def __init__(self, funcionario_nome, funcionario_senha, funcionario_cpf, funcionario_cep, funcionario_email, funcionario_ddi, funcionario_ddd, funcionario_telefone, funcionario_cargo):
         self.funcionario_nome = funcionario_nome
         self.funcionario_senha = funcionario_senha
@@ -33,6 +36,7 @@ class Funcionario(CrudBase):
     def validate(self):
         erros = []
 
+        #valida cada campo da tabela
         validacoes = [
             Validator.validar_nome(self.funcionario_nome, "funcionario_nome"),
             Validator.validar_senha(self.funcionario_senha, "funcionario_senha"),
@@ -45,7 +49,7 @@ class Funcionario(CrudBase):
             Validator.validar_telefone(self.funcionario_telefone, "funcionario_telefone"),
             Validator.validar_cargo(self.funcionario_cargo, "funcionario_cargo")
         ]
-        
+        #se der algum erro
         for itens in validacoes:
             if not itens['valida']:
                 erros.append(itens["mensagem"])
@@ -53,24 +57,26 @@ class Funcionario(CrudBase):
         return erros
 
     
-
+    #função pra logar no sistema
     @classmethod
     def login(cls, email, senha):
 
-        conexao = Database.connect()
-        cursor = conexao.cursor(dictionary=True)
+        conexao = Database.connect() #conecta no banco
+        cursor = conexao.cursor(dictionary=True) #objeto
 
         try:
+            #query select pra encontrar se existe algum funcionário com aquelas informações
             sql = """
                 SELECT *
                 FROM funcionario
                 WHERE funcionario_email = %s
             """
+            
+            cursor.execute(sql, (email,)) #executa o comando select
+            usuario = cursor.fetchone() #retorna as informações após o comando do sql
 
-            cursor.execute(sql, (email,))
-            usuario = cursor.fetchone()
-
-            if usuario and verificar_senha(
+            #verifica se o usuario existe e se a senha está correta
+            if usuario and verificar_senha( #aqui compara a senha digitada com a senha criptografada
                 senha,
                 usuario["funcionario_senha"]
             ):
@@ -78,20 +84,21 @@ class Funcionario(CrudBase):
 
             return None
 
-        finally:
-            cursor.close()
+        finally: #fecha o objeto e a conexão
+            cursor.close() 
             conexao.close()
 
     #Função para listar todos os funcionarios registrados
     @classmethod
     def funcionario_listagem(cls):
-        conexao = Database.connect()
-        cursor = conexao.cursor(dictionary=True)
+        conexao = Database.connect() #conecta no banco
+        cursor = conexao.cursor(dictionary=True) #objeto
         try:
+            #seleciona tudo de funcionario
             sql = "SELECT * FROM funcionario"
-            cursor.execute(sql)
-            return cursor.fetchall()
-        finally:
+            cursor.execute(sql) #executa o comando sql do select
+            return cursor.fetchall() #retorna os dados após o comando select
+        finally: # fecha o objeto e a conexão
             cursor.close()
             conexao.close()
 
@@ -99,65 +106,69 @@ class Funcionario(CrudBase):
     #Função para deletar com segurança
     @classmethod
     def safe_delete(cls, id):
-        fornecedor = cls.find_by_id(id)
-        if not fornecedor:
+        funcionario = cls.find_by_id(id) #procura o id do funcionario
+        if not funcionario: #se não achar o id do funcionario
             raise ValueError("Funcionario não encontrado.")
-        if cls.has_related_records(id):
+        if cls.has_related_records(id): #verifica se esse fucnionario está vinculado em alguma tabela
             raise ValueError("Não é possível excluir o funcionario porque ele está vinculado a outros serviços.")
         cls.delete(id)
 
     #Procura se o funcionario em questão está relacionado com algum pedido de entrada
     @classmethod
     def has_related_records(cls, id):
-        conexao = Database.connect()
+        conexao = Database.connect() #conecta no banco
         cursor = conexao.cursor()
         try:
+            #conta quantas vezes existe o campo funcionario_id na tabela uso_emilhadeira
             queries = [
                 "SELECT COUNT(*) FROM uso_empilhadeira WHERE funcionario_id = %s"
             ]
             total = 0
-            for sql in queries:
-                cursor.execute(sql, (id,))
-                total += cursor.fetchone()[0]
-            return total > 0
-        finally:
+            for sql in queries: #percorre todos os comandos de dentro a tabela, porém nesse caso, só uma vez
+                cursor.execute(sql, (id,)) #executa cada comando passando o id
+                total += cursor.fetchone()[0] #pega a quantidade de vezes que encontrou do retorno e soma no total
+            return total > 0 #se for maior que zero=True | se for menor que zero=False
+        finally: #fecha o objeto e a conexão
             cursor.close()
             conexao.close()
 
     #Procura no banco algum funcionário com o mesmo CPF
     @classmethod
     def cpf_existente(cls, cpf):
-        conexao = Database.connect()
-        cursor = conexao.cursor(dictionary=True, buffered=True)
+        conexao = Database.connect() #conecta no banco
+        cursor = conexao.cursor(dictionary=True, buffered=True) #objeto
         try:
+            #procura se existe um funcionario com um cpf especifico
             sql = f"SELECT * FROM {cls.table} WHERE funcionario_cpf = %s"
-            cursor.execute(sql, (cpf,))
-            return cursor.fetchone()
-        finally:
+            cursor.execute(sql, (cpf,)) #executa o query
+            return cursor.fetchone() #retorna o resultado da query
+        finally: # fecha o objeto e a conexão
             cursor.close()
             conexao.close()
     
     #Procura no banco algum funcionário com o mesmo email
     @classmethod
     def email_existente(cls, email):
-        conexao = Database.connect()
-        cursor = conexao.cursor(dictionary=True, buffered=True)
+        conexao = Database.connect() #conecta no banco
+        cursor = conexao.cursor(dictionary=True, buffered=True) #objeto
         try:
+            #procura se existe um funcionario com um email especifico
             sql = f"SELECT * FROM {cls.table} WHERE funcionario_email = %s"
-            cursor.execute(sql, (email,))
-            return cursor.fetchone()
-        finally:
+            cursor.execute(sql, (email,)) #executa o query
+            return cursor.fetchone() #retorna o resultado da query
+        finally: # fecha o objeto e a conexão
             cursor.close()
             conexao.close()
 
     
     def inserir_funcionario(self):
 
-        self.funcionario_senha = gerar_hash_senha(
+        self.funcionario_senha = gerar_hash_senha( #pega a senha digitada e criptografa ela
             self.funcionario_senha
         )
-
+        #insere no banco a senha já criptografada
         return self.insert()
+
 
     def atualizar_funcionario(self, id_funcionario, dados):
         senha = dados.get("funcionario_senha")
@@ -170,32 +181,33 @@ class Funcionario(CrudBase):
 
         self.update(id_funcionario, dados)
 
+
     @classmethod
     def autenticar(cls, email, senha):
         """
         Verifica login do funcionário.
         """
 
-        conexao = Database.connect()
+        conexao = Database.connect() #conecta no banco
 
-        cursor = conexao.cursor(dictionary=True)
+        cursor = conexao.cursor(dictionary=True) #objeto
 
         try:
-
+            #query select pra encontrar se existe algum funcionário com aquelas informações
             sql = f"""
                 SELECT *
                 FROM {cls.table}
                 WHERE funcionario_email = %s
             """
 
-            cursor.execute(sql, (email,))
-            funcionario = cursor.fetchone()
+            cursor.execute(sql, (email,)) #executa o comando select
+            funcionario = cursor.fetchone() #retorna as informações após o comando do sql
 
 
-            if not funcionario:
+            if not funcionario: #se nao tiver nada em funcionario
                 return None
 
-            if verificar_senha(
+            if verificar_senha( #se a senha digitada bater com a criptografada
                 senha,
                 funcionario["funcionario_senha"]
             ):
@@ -203,7 +215,7 @@ class Funcionario(CrudBase):
 
             return None
 
-        finally:
+        finally: #fecha o objeto e a conexão
 
             cursor.close()
             conexao.close()
