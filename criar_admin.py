4caa3fd79fa42b814c7fca31aa1b5516499566b1
voituregestaodeestoque <243357527+Ryan-Top@users.noreply.github.com>
2026-07-28 11:14:44 -0300

    1 perfil de admin só

--- criar_admin.py
+++ criar_admin.py
@@ -1,5 +1,33 @@
 from models.funcionario import Funcionario
 #importa a classe funcionario junto com as funções
+from core.database import Database
+
+def select_admin():
+    conexao = Database.connect() #conecta no banco
+
+    cursor = conexao.cursor(dictionary=True) #objeto
+
+    try:
+        #query select pra encontrar se existe algum funcionário com aquelas informações
+        sql = f"""
+            SELECT *
+            FROM funcionario
+            WHERE funcionario_nome = 'Administrador'
+            """
+
+        cursor.execute(sql) #executa o comando select
+        funcionario = cursor.fetchall() #retorna as informações após o comando do sql
+        if not funcionario: #se nao tiver nada em funcionario
+            return None
+
+        if funcionario:
+            return funcionario
+
+        return None
+
+    finally: #fecha o objeto e a conexão
+        cursor.close()
+        conexao.close()
 
 dados = {
     "funcionario_nome": "Administrador",
@@ -12,10 +40,13 @@ dados = {
     "funcionario_telefone": "123456789",
     "funcionario_cargo": "Admin"
 }
-#dados de admin para iniciar o sistema
 
-funcionario = Funcionario(**dados)
-#juntando os dados dentro da classe funcionario pra inserir no banco
-funcionario.inserir_funcionario()
-
-print("Usuário administrador criado com sucesso.")
+funcionario=select_admin()
+if funcionario:
+    print("Admin já cadastrado")
+else:
+    #dados de admin para iniciar o sistema
+    funcionario = Funcionario(**dados)
+    #juntando os dados dentro da classe funcionario pra inserir no banco
+    funcionario.inserir_funcionario()
+    print("Usuário administrador criado com sucesso.")
