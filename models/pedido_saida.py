e317693d52c7c790181bae867bf6342821766595
mclarabelli <maria.belli@aluno.senai.br>
2026-08-11 10:04:48 -0300

    Update pedido_saida.py

--- models/pedido_saida.py
+++ models/pedido_saida.py
@@ -114,7 +114,7 @@ class Pedido_saida(CrudBase):
                 #Verificar se há estoque suficiente
                 if estoque["estoque_quantidade"] < item["detalhe_saida_quantidade"]:
                     conexao.rollback()
-                    return f"Estoque insuficiente para o item {item['produto_id']}."
+                    return f"Estoque insuficiente para o item {item['produto_nome']}."
 
                 
                 nova_quantidade = estoque["estoque_quantidade"] - item["detalhe_saida_quantidade"]
@@ -212,7 +212,7 @@ class Pedido_saida(CrudBase):
                 
                 if estoque['estoque_quantidade'] < detalhe["detalhe_saida_quantidade"]:
                     raise ValueError (
-                        f"Estoque insuficiente para o produto {detalhe['produto_id']}"
+                        f"Estoque insuficiente para o produto {detalhe['produto_nome']}"
                     )
 
                 nova_quantidade = (
